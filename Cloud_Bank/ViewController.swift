d38a3075e0877d9dfcb3b4fd771167b6ac9e451b
JTzBallin515 <jeremy.rodriguez@marquette.edu>
2017-12-16 10:27:26 -0600

    Initial Commit

---
+++ Cloud_Bank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cloud_Bank
+//
+//  Created by Jeremy Rodriguez on 12/16/17.
+//  Copyright © 2017 Jeremy Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
